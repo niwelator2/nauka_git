7dfed3861a4f1cd2471977e0d8e2077338062dba
user22-33 <kacperuoos2522@gmail.com>
2024-09-30 11:19:02 +0200

    KacperM main.cpp

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    cout<<"Podaj a: "<<endl;
+    cin >> a;
+    cout<<"Podaj b: "<<endl;
+    cin >> b;
+    int c = a+b;
+    cout<<"Suma liczb wynosi: "<<c<<endl;
+}
